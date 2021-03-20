a6fb367cf215591cac1c4ac159bcd4c398764dba
Костянтин <obidos161@gmail.com>
2021-03-20 14:17:47 +0200

    refreshen token

--- callback.php
+++ callback.php
@@ -24,6 +24,14 @@ $data = http_build_query($data_array);
 
 echo "<br/>";
 
+
+
+
+
+
+
+
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
@@ -36,10 +44,39 @@ $resp = curl_exec($ch);
 if ($e = curl_error($ch)) {
     echo $e;
 } else {
+    //$token = $_POST['refresh_token'];
     $decode = json_decode($resp);
     foreach ($decode as $key => $value) {
+        if ($key == 'refresh_token') {
+            $token = $value;
+        }
         echo $key . ':' . $value . '<br>';
     }
 }
 
 curl_close($ch);
+
+if (isset($token)) {
+
+    $URL = "https://oauth.accounting.sage.com/revoke";
+    $data_true = array(
+        'client_id' => $CLIENT_ID,
+        'client_secret' => $CLIENT_SECRET,
+        'token' => $token
+    );
+
+    $data_two = http_build_query($data_true);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $URL);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_two);
+    //curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded"));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $responce = curl_exec($ch);
+
+    $decode = json_decode($responce);
+
+    print_r($decode);
+    curl_close($ch);
+}
